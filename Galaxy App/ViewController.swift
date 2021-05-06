125aeb3b0a907e9b5e40097cd1065936161835f0
ko kyaw <kyaw.codes@gmail.com>
2021-05-06 20:20:13 +0630

    Initial commit. Set up pod and configure zero storyboard project

---
+++ Galaxy App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Galaxy App
+//
+//  Created by Ko Kyaw on 06/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+
